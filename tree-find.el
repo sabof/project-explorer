22fa47a4f71f49c5aa48438c2bbf84df0ae2fc8d
sabof <esabof@gmail.com>
2013-09-22 15:52:08 +0100

    Added GPL header

--- tree-find.el
+++ tree-find.el
@@ -1,4 +1,33 @@
-;; -*- lexical-binding: t -*-
+;;; tree-find.el --- Find-based project explorer -*- lexical-binding: t -*-
+;;; Version: 0.1
+;;; Author: sabof
+;;; URL: https://github.com/sabof/tree-find
+
+;;; Commentary:
+
+;; The project is hosted at https://github.com/sabof/tree-find
+;; The latest version, and all the relevant information can be found there.
+
+;;; License:
+
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
 
 (require 'cl-lib)
 (require 'es-lib)
@@ -18,7 +47,7 @@
       (error "This version of tree-find requires bash"))
     )
   "The \"backend\" for tree-find.
-  Can be a string, or a function.
+  Can be a string, or a function \(WIP\).
 
   The string will be executed in bash, and it's output should be a
   list of files separated by newlines.
